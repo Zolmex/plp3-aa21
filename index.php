f1a7a532419e7c7f56966ac8c41be5f2aed6d0cb
Zolmex <zolmexyt@gmail.com>
2025-10-22 18:45:32 -0300

    Saludo con metodo GET

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-$nombre="";
-$edad=0;
-$hobby="";
-$mensaje= "";
-$respuesta=false;
+$nombre = "";
+$edad = 0;
+$hobby = "";
+$mensaje = "";
+$respuesta = false;
+$saludo = "";
 
-if ($_SERVER['REQUEST_METHOD'] == "POST"){
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $nombre = htmlspecialchars($_POST["name"]);
     $edad = htmlspecialchars($_POST["age"]);
     $hobby = htmlspecialchars($_POST["hobby"]);
@@ -13,16 +14,16 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
 
     if ($edad > 50) {
         $mensaje = "Usted tiene un perfil de senior developer.";
-    }
-    else if ($edad > 30) {
+    } else if ($edad > 30) {
         $mensaje = "Usted tiene un perfil de intermediate developer.";
-    }
-    else if ($edad > 20) {
+    } else if ($edad > 20) {
         $mensaje = "Usted tiene un perfil de junior developer.";
-    }
-    else {
+    } else {
         $mensaje = "Usted tiene un perfil de trainee.";
     }
+}
+else if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    $saludo = htmlspecialchars($_GET["saludo"]);
 }
 ?>
 <!DOCTYPE html>
@@ -56,6 +57,12 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
             <p id="tarjeta-hobby"><?php echo $hobby ?></p>
             <p id="tarjeta-mensaje"><?php echo $mensaje ?></p>
         </div>
+        <?php endif; ?>
+        <?php if ($saludo != ""): ?>
+            <div id="saludo">
+                <h1>Mensaje del cliente</h1>
+                <p><?php echo $saludo ?></p>
+            </div>
         <?php endif; ?>
     </main>
 </body>
